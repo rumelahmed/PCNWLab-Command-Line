b64e7299bddfa2add5dbd0051d05956fa5832412
Rumel <rumelahmed@pursuit.org>
2019-09-25 10:29:17 -0400

    answer pset1

--- pset1.js
+++ pset1.js
@@ -1,40 +1,62 @@
 /******************
-PROBLEM 1:
+PROBLEM 1:
 
-Follow the following steps ONLY using your command line:
+Follow the following steps ONLY using your command line:
 
-1. Navigate to your Desktop directory.
-2. Create a directory called my_first_direc.
-3. Enter the my_first_direc directory.
-4. Create a file called first.js.
-5. Create a folder called assignments.
-6. Move first.js into assignments.
-7. Go into the assignments folder.
-8. Delete first.js.
-9. Go up to the my_first_direc directory.
-10. Delete the assignments folder.
-11. Go up to the Desktop directory.
-12. Delete the test folder.
-
+1. Navigate to your Desktop directory.
+cd Desktop
+2. Create a directory called my_first_direc.
+mkdir my_first_direc
+3. Enter the my_first_direc directory.
+cd my_first_direc
+4. Create a file called first.js.
+touch first.js
+5. Create a folder called assignments.
+mkdir assignments
+6. Move first.js into assignments.
+mv first.js assignments
+7. Go into the assignments folder.
+cd assignments
+8. Delete first.js.
+rm first.js
+9. Go up to the my_first_direc directory.
+cd ..
+10. Delete the assignments folder.
+rm -r assignments
+11. Go up to the Desktop directory.
+cd ..
+12. Delete the test folder.
+rm -r my_first_direc
 ******************/
 
 
 
 /******************
-PROBLEM 2:
+PROBLEM 2:
 
-Follow the following steps ONLY using your command line:
+Follow the following steps ONLY using your command line:
 
-1. Navigate to the inside of your Documents folder
-3. Create a file called foo.js
-4. Open the file with a text editor, (Atom/VSCode)
-5. Add the following text to the file: console.log('hello')
-6. Save and exit the file
-7. Open the file again with a text editor, (Atom/VSCode)
-8. Add the following in a new line: hellohellohello
-9. Exit without saving
+1. Navigate to the inside of your Documents folder
+cd ..
+cd Documents
+3. Create a file called foo.js
+touch foo.js
+4. Open the file with a text editor, (Atom/VSCode)
+open foo.js
+5. Add the following text to the file: 
+echo "hello">>  foo.js 
+6. Save and exit the file
+cmd + S
+
+7. Open the file again with a text editor, (Atom/VSCode)
+cd Documents
+open foo.js
+8. Add the following in a new line: hellohellohello
+echo "hellohellohello">> foo.js
+9. Exit without saving
 
 ******************/
+
 
 
 
